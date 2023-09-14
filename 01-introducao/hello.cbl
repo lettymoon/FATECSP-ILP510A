932ec9e5856e0d1fd5d8945f819e1c2bd6c9d9ce
lettymoon <lettinwonderland@gmail.com>
2023-09-14 12:09:53 -0300

    aula 01

--- 01-introducao/hello.cbl
+++ 01-introducao/hello.cbl
@@ -1,5 +1,18 @@
-        identification division.
-        program-id. hello.
-        procedure division.
-           display "hello".
+       identification division.
+       program-id. hello.
+       author. Letty
+       installation. my-pc
+       date-written. 26/08/2023
+       date-compiled. 26/08/2023
+
+       environment division.
+       configuration section.
+           object-computer. laptop-letty.
+           special-names. decimal-point is comma.
+
+       procedure division.
+           perform hello.
            stop run.
+
+           hello. 
+               display "hello world!".
